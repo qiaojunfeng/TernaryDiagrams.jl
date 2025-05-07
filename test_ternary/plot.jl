16da4cabd8ab068361375c042899e1acfbcc7877
Junfeng Qiao <qiaojunfeng@outlook.com>
2025-05-07 23:29:18 +0200

    Add colorbar

--- test_ternary/plot.jl
+++ test_ternary/plot.jl
@@ -9,13 +9,11 @@ a2 = a2[1:20]
 a3 = a3[1:20]
 mus = mus[1:20]
 
-fig = Figure(size = (900, 600))
+fig = Figure(size = (800, 600))
 ax = Axis(fig[1, 1], aspect=DataAspect())
 
-ternarycontourf!(ax, a1, a2, a3, mus;
-    levels = 10,
-    colormap = reverse(Makie.ColorSchemes.Spectral),
-)
+colormap = reverse(Makie.ColorSchemes.Spectral)
+ternarycontourf!(ax, a1, a2, a3, mus; levels = 10, colormap)
 
 p = ternaryaxis!(
     ax;
@@ -26,6 +24,8 @@ p = ternaryaxis!(
     # hide_triangle_labels = true,
     hide_arrows = true,
 )
+
+Colorbar(fig[1, 2]; colormap)
 
 xlims!(ax, -0.2, 1.2) # to center the triangle
 ylims!(ax, -0.3, 1.1) # to center the triangle
